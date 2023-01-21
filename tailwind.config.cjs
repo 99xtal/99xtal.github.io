5f7e889b52262b520e07e6b8adc37f4c465e8683
Joe Rybarczyk <joerybarczyk99@gmail.com>
2023-01-21 03:09:54 -0500

    Create navbar and hero screen template

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,astro}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'fredoka': ['Fredoka'],
+        'khula': ['Khula']
+      }
+    },
+  },
+  plugins: [],
+}
